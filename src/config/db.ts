e3f180ca763f3bdbb7fcd2ca256623c9cda098b7
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-04-20 02:17:04 +0600

    bookings table created

--- src/config/db.ts
+++ src/config/db.ts
@@ -13,7 +13,7 @@ const initDB = async() => {
         // users table
         await pool.query(`
                 CREATE TABLE IF NOT EXISTS users(
-                    id SERIAL NOT NULL,
+                    id SERIAL PRIMARY KEY,
                     name VARCHAR(150) NOT NULL,
                     email VARCHAR(200) UNIQUE NOT NULL,
                     password TEXT NOT NULL,
@@ -25,12 +25,25 @@ const initDB = async() => {
         // vehicle table
         await pool.query(`
                 CREATE TABLE IF NOT EXISTS vehicles(
-                    id SERIAL NOT NULL,
+                    id SERIAL PRIMARY KEY,
                     vehicle_name VARCHAR(250) NOT NULL,
                     type VARCHAR(20) NOT NULL,
                     registration_number VARCHAR(150) UNIQUE NOT NULL,
                     daily_rent_price NUMERIC NOT NULL CHECK (daily_rent_price > 0),
                     availability_status VARCHAR(15) NOT NULL DEFAULT 'available' CHECK (availability_status IN ('available', 'booked'))
+                )
+            `);
+
+        // booking table
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS bookings(
+                    id SERIAL NOT NULL,
+                    customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+                    vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+                    rent_start_date DATE NOT NULL,
+                    rent_end_date DATE NOT NULL,
+                    total_price NUMERIC NOT NULL CHECK (total_price > 0),
+                    status VARCHAR(20) NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'cancelled', 'returned'))
                 )
             `);
 

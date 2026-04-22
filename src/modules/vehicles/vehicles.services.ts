b37323a3396651890ba0b5451b1cae0770596a92
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-04-23 04:10:55 +0600

    fixing email issue

--- src/modules/vehicles/vehicles.services.ts
+++ src/modules/vehicles/vehicles.services.ts
@@ -9,6 +9,7 @@ const createVehicle = async(payload: Vehicle) => {
     const result = await pool.query(`INSERT INTO vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status]);
 
     const vehicle = result.rows[0]
+    // console.log(vehicle)
     return vehicle
 }
 
